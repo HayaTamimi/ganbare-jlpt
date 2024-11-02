24580f42b17778172dc1ee5d6faf65553c1b7539
HayaTamimi <145218954+HayaTamimi@users.noreply.github.com>
2024-11-02 23:33:28 +0300

    loging done

--- src/components/user/UserLogin.js
+++ src/components/user/UserLogin.js
@@ -1,33 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserLogin.css";
+import axios from "axios";
 
 
 export default function UserLogin() {
+  const [userLogIn, setUserLogIn] = useState({
+    email: "",
+    password: "",
+  });
+
+  function onChangeHandlerEmailLogin(event) {
+    setUserLogIn({ ...userLogIn, email: event.target.value });
+  }
+
+  function onChangeHandlerPasswordLogin(event) {
+    setUserLogIn({ ...userLogIn, password: event.target.value });
+  }
+
+function logInUser() {
+  const userUrlLogIn = "http://localhost:5171/api/v1/users/signIn";
+
+  axios
+    .post(userUrlLogIn, userLogIn)
+    .then((res) => {
+      console.log(res, "response from log in");
+      if (res.status === 200) {
+        localStorage.setItem("token", res.data);
+      }
+    })
+    .catch((error) => console.log(error));
+}
+
+
   return (
     <div className="container">
       <div className="login">
         <label htmlFor="email">Email:</label>
 
         <form>
-          <input
-            type="email"
-            id="email"
-            //  onChange={onChangeHandlerEmail}
-          />
+          <input type="email" id="email" onChange={onChangeHandlerEmailLogin} />
         </form>
         <label htmlFor="password">Password:</label>
 
         <form>
-          <input
-            type="password"
-            // onChange={onChangeHandlerPassword}
-          />
+          <input type="password" onChange={onChangeHandlerPasswordLogin} />
         </form>
 
-        <button
-          className="user-btn"
-          //  onClick={registerNewUser}
-        >
+        <button className="user-btn" onClick={logInUser}>
           Sign In
         </button>
       </div>
