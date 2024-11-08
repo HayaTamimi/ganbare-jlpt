1500e8d55ecb6ed4ad83cdb42e8d6235a521be97
HayaTamimi <145218954+HayaTamimi@users.noreply.github.com>
2024-11-08 21:40:13 +0300

    added about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./About.css";
-import Quiz from "../components/quiz/Quiz";
-
 
 export default function About() {
 
   return (
     <div className="container">
-      <div>
-        <p className="about"> testing page for now</p>
+      <div className="about">
+        <div className="about-b">About Us</div>
+        <div className="about-p">Welcome to our JLPT Quiz App! We're here to help you ace your
+        Japanese Language Proficiency Test. Practice with a variety of question
+        types, track your progress, and boost your confidence. Let's learn
+        Japanese together!</div>
+        
       </div>
     </div>
   );
