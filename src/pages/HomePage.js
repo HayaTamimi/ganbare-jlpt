a35372d662640195759b8a43bd5af4c6134c01f3
HayaTamimi <145218954+HayaTamimi@users.noreply.github.com>
2024-11-13 10:05:04 +0300

    x

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Main from "../components/main/Main"
 import Footer from '../components/footer/Footer'
-import Quiz from '../components/quiz/Quiz';
 
 export default function Homepage() {
   return (
